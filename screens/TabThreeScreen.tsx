04c43d3e80f0548d0922253f45f9f95aee3183aa
GoranTopic <telegonicaxx@live.com>
2021-10-21 02:14:59 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,94 +1,94 @@
-				});
-			padding: 3,
-		color: 'white',
-		alignSelf: 'center',
-		alignSelf: 'center',
-		alignSelf: 'center',
-										<Text style={styles.text}>{item}</Text>
-			fontSize: 14,
-		width: "95%",
-								data={ state.logger }
-	},
-	},
-	},
-	},
-		return (
-			borderColor: 'gold',
-						<FlatList
-  container: {
-    fontWeight: 'bold',
-		paddingRight: 15,
-		paddingTop: '6%',
-								onSubmitEditing={handleSubmit}
-								style={styles.searchInput}
-		}
-		);
-		borderBottomColor: 'gold',
-
-
-
-
-
-
+import * as React from 'react';
+import { Button, StyleSheet, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View, TextInput } from '../components/Themed';
+import Reservation from '../components/Reservation';
 
 export default function TabThreeScreen({ state, dispatch, navigation }) {
-import * as React from 'react';
-			width: 10,
-    flex: 1,
-import { Button, StyleSheet, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
-		const handleSeachInput = (text)=>  setText(text); 
-    fontSize: 11,
-			borderRadius: 8,
-import { Text, View, TextInput } from '../components/Themed';
-								defaultValue={text}
-			borderWidth: 0.2,
-    width: '100%',
-	text:{
-			backgroundColor: 'white'
-						</View>
-    height: 1,
-		paddingLeft: 15,
-			marginVertical: 5,
-						type: 'ADD_LOG',
-	separator: {
-								</View>
-						<TextInput
-import EditScreenInfo from '../components/EditScreenInfo';
-			borderRadius: 30,
-    justifyContent: 'center',
-								onChangeText={handleSeachInput}
-}
-	searchInput: {
-								placeholder="Message..."
-});
-				dispatch({
-								renderItem={ 
-						/>
-												style={styles.eventContainer} >
-						}/>
-		borderWidth: 0.2,
-		const handleSubmit = () => { 
-	dot:{
-		paddingVertical: 3,
-										({item}) =>  
-	eventContainer:{
-				<View style={styles.container}>
-				setText('');
-const styles = StyleSheet.create({
-			margin:5
-						<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-								keyExtractor={(item, index) => index.toString()}
-		margin: 5,
-  title: {
-			height: 10,
-								placeholderTextColor="gray"
-								styles={styles.reservList}
-		
-			alignSelf: 'flex-start',
+
 		const [ text, setText ] = React.useState('');
+
+		const handleSeachInput = (text)=>  setText(text); 
+
+		const handleSubmit = () => { 
+				dispatch({
+						type: 'ADD_LOG',
 						payload:{ log: text, },
-import Reservation from '../components/Reservation';
-  },
-  },
-  },
+				});
+				setText('');
+		}
+		
+		return (
+				<View style={styles.container}>
+						<FlatList
+								styles={styles.reservList}
+								data={ state.logger }
+								keyExtractor={(item, index) => index.toString()}
+								renderItem={ 
+										({item}) =>  
 										<View key={item}
+												style={styles.eventContainer} >
+										<Text style={styles.text}>{item}</Text>
+								</View>
+						}/>
+						<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+						<TextInput
+								style={styles.searchInput}
+								placeholder="Message..."
+								placeholderTextColor="gray"
+								onChangeText={handleSeachInput}
+								onSubmitEditing={handleSubmit}
+								defaultValue={text}
+						/>
+						</View>
+		);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+		paddingTop: '6%',
+    justifyContent: 'center',
+  },
+  title: {
+		alignSelf: 'center',
+    fontSize: 11,
+    fontWeight: 'bold',
+  },
+	dot:{
+			width: 10,
+			height: 10,
+			borderRadius: 30,
+			backgroundColor: 'white'
+	},
+	eventContainer:{
+			padding: 3,
+			marginVertical: 5,
+			borderWidth: 0.2,
+			borderColor: 'gold',
+			borderRadius: 8,
+			alignSelf: 'flex-start',
+	},
+	text:{
+			fontSize: 14,
+			margin:5
+	},
+	searchInput: {
+		alignSelf: 'center',
+		width: "95%",
+		margin: 5,
+		paddingVertical: 3,
+		paddingLeft: 15,
+		paddingRight: 15,
+		color: 'white',
+		borderBottomColor: 'gold',
+		borderWidth: 0.2,
+	},
+	separator: {
+		alignSelf: 'center',
+    height: 1,
+    width: '100%',
+  },
+});
+
+
