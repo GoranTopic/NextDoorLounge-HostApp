bc6d5567645e3e65903ef42ae42274ac1006db71
GoranTopic <telegonicaxx@live.com>
2021-10-21 01:34:49 -0700

    made some changes

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,94 +1,94 @@
-import * as React from 'react';
-import { Button, StyleSheet, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View, TextInput } from '../components/Themed';
-import Reservation from '../components/Reservation';
-
-export default function TabThreeScreen({ state, dispatch, navigation }) {
-
-		const [ text, setText ] = React.useState('');
-
-		const handleSeachInput = (text)=>  setText(text); 
-
-		const handleSubmit = () => { 
-				dispatch({
-						type: 'ADD_LOG',
-						payload:{ log: text, },
-				});
-				setText('');
-		}
-		
-		return (
-				<View style={styles.container}>
-						<FlatList
-								styles={styles.reservList}
-								data={ state.logger }
-								keyExtractor={(item, index) => index.toString()}
-								renderItem={ 
-										({item}) =>  
-										<View key={item}
-												style={styles.eventContainer} >
-										<Text style={styles.text}>{item}</Text>
-								</View>
-						}/>
-						<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-						<TextInput
-								style={styles.searchInput}
-								placeholder="Message..."
-								placeholderTextColor="gray"
-								onChangeText={handleSeachInput}
-								onSubmitEditing={handleSubmit}
-								defaultValue={text}
-						/>
-						</View>
-		);
-}
-
-const styles = StyleSheet.create({
   container: {
-    flex: 1,
-		paddingTop: '6%',
-    justifyContent: 'center',
-  },
-  title: {
-		alignSelf: 'center',
+								onSubmitEditing={handleSubmit}
     fontSize: 11,
-    fontWeight: 'bold',
-  },
-	dot:{
-			width: 10,
-			height: 10,
-			borderRadius: 30,
-			backgroundColor: 'white'
-	},
-	eventContainer:{
 			padding: 3,
-			marginVertical: 5,
-			borderWidth: 0.2,
-			borderColor: 'gold',
-			borderRadius: 8,
-			alignSelf: 'flex-start',
-	},
-	text:{
-			fontSize: 14,
-			margin:5
-	},
 	searchInput: {
-		alignSelf: 'center',
-		width: "95%",
-		margin: 5,
-		paddingVertical: 3,
-		paddingLeft: 15,
-		paddingRight: 15,
-		color: 'white',
+const styles = StyleSheet.create({
+						<FlatList
+						}/>
+		return (
+				});
+						</View>
+			marginVertical: 5,
+			fontSize: 14,
+								defaultValue={text}
+		}
+										<View key={item}
+			borderColor: 'gold',
+import { Text, View, TextInput } from '../components/Themed';
 		borderBottomColor: 'gold',
-		borderWidth: 0.2,
 	},
-	separator: {
+	},
+	},
+	},
+			borderWidth: 0.2,
+						type: 'ADD_LOG',
+import Reservation from '../components/Reservation';
+												style={styles.eventContainer} >
+}
+								placeholder="Message..."
+				setText('');
 		alignSelf: 'center',
+		alignSelf: 'center',
+		alignSelf: 'center',
+		paddingRight: 15,
+	text:{
+		color: 'white',
+			width: 10,
+			borderRadius: 8,
+		const handleSubmit = () => { 
+    justifyContent: 'center',
+				<View style={styles.container}>
     height: 1,
-    width: '100%',
-  },
+								keyExtractor={(item, index) => index.toString()}
+		width: "95%",
+				dispatch({
+								style={styles.searchInput}
+		const handleSeachInput = (text)=>  setText(text); 
+			alignSelf: 'flex-start',
 });
+								data={ state.logger }
+		margin: 5,
+    fontWeight: 'bold',
+		paddingTop: '6%',
+	dot:{
+			borderRadius: 30,
+	separator: {
+			height: 10,
+		borderWidth: 0.2,
+						<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+		);
+						<TextInput
+		paddingVertical: 3,
+								onChangeText={handleSeachInput}
+										({item}) =>  
+								styles={styles.reservList}
+import * as React from 'react';
+  },
+  },
+  },
+    width: '100%',
+	eventContainer:{
+								placeholderTextColor="gray"
+						/>
+  title: {
+								renderItem={ 
+		const [ text, setText ] = React.useState('');
+export default function TabThreeScreen({ state, dispatch, navigation }) {
+import EditScreenInfo from '../components/EditScreenInfo';
+						payload:{ log: text, },
+    flex: 1,
+import { Button, StyleSheet, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+								</View>
 
 
+
+
+
+
+
+			margin:5
+			backgroundColor: 'white'
+										<Text style={styles.text}>{item}</Text>
+		paddingLeft: 15,
+		
